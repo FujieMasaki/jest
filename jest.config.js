992aff7a6912ccffcc20097244c8e60fcfe7e7fa
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2024-02-10 13:22:24 +0900

    jestの環境構築

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  // jestでtypescriptのコードをテストするための設定
+  testEnvironment: "node",
+  // jestがnodeの実行環境で実行される
+};
