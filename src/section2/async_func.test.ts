4ad6192bed489e96da46c9f4e1985e479b2943cd
FujieMasaki <ffffffzzzzzeeee@gmail.com>
2024-02-11 14:56:58 +0900

    非同期処理が失敗する時のtest追加

--- src/section2/async_func.test.ts
+++ src/section2/async_func.test.ts
@@ -8,8 +8,11 @@ it("delay関数が指定した秒数でメッセージを返す", async () => {
 });
 
 // 非同期関数が失敗する時
-it("delay関数が指定した秒数でメッセージを返す", async () => {
-  // 非同期関数を呼び出す際はconstでresultを定義し、関数の呼び出しにawaitをつける
-  const result = await delay("Hello jest", 1000);
-  expect(result).toBe("Hello jest");
+it("timeがマイナスの場合はエラーが発生する", async () => {
+  // eにエラーオブジェクトが渡される
+  try {
+    const result = await delay("Hello jest", -1);
+  } catch (e: any) {
+    expect(e.message).toBe("timeは0以上で指定してください");
+  }
 });
